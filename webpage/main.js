098abadd08a2970fe7e5825e17c4358cedd5f18d
ejnarvala <ejnar123@gmail.com>
2017-07-26 12:11:43 -0400

    fix in main.js

--- webpage/main.js
+++ webpage/main.js
@@ -16,7 +16,7 @@ function createWindow () {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
   mainWindow.webSecurity = false;
 
   // Emitted when the window is closed.
