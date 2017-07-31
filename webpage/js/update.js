9d64319cf422a2b81e9f7cf3f7bc7952367f7dd8
ejnarvala <ejnar123@gmail.com>
2017-07-31 10:00:03 -0400

    Documentation on update.js added, some code cleaning

--- webpage/js/update.js
+++ webpage/js/update.js
@@ -3,40 +3,52 @@ var chart_interval, table_interval, ip_interval;
 var upper_thresh = 38.00;
 var lower_thresh = 10.00;
 var chart_refresh_time = 180000; //3 minutes
-var live_table_refresh_time = 5000; //3 seconds
-var ip_refresh_time = 300000; //15 minutes
+var live_table_refresh_time = 5000; //5 seconds
+var ip_refresh_time = 100000; //5 minutes
 var colors =['#1F271B','#19647E','#28AFB0','#F4D35E','#EE964B','#1EB24F','#0B4F6C','#01BAEF','#757575']
 var day = 0;
+var days_to_keep_live_data = 5;
+var num_ip_colums = 12;
+
+
+
+
+var points = [];
+var c = 0;
+var datetime, date, time;
 function update_chart_table() {
+    //Make a request to the arduino for json data containing temperature readings
     $.ajax({
         url: "http://192.168.100.100/json",
         data: { tag: 'GetDataFromArduino'},
         dataType: "json",
         timeout: 3000,
-        success: function(data){
-            var points = [];
-            var c = 0;
-            var datetime = data.date;
-            datetime = datetime.split(" "); //[date, time]
-            var date = datetime[0].split("-");
-            var time = datetime[1].split(":");
-            datetime = Date.UTC(date[2], date[0] - 1, date[1], time[0], time[1], time[2]);
-            if(day == 0){
+        success: function(data){ //if a request is successful, process the sensor data to add to live plot
+            //data is in formate: {date, S0, S1, ...}
+            points = [];
+            c = 0; //counter variable that will later becme number of sensors
+            datetime = data.date;
+            datetime = datetime.split(" "); //Splits into this: [date, time]
+            date = datetime[0].split("-"); //Splits date
+            time = datetime[1].split(":"); //splits time
+            datetime = Date.UTC(date[2], date[0] - 1, date[1], time[0], time[1], time[2]); //convert date to UTC format for plotting
+            if(day == 0){ //if first run, set the date to current date
                 day = date[1];
             }
-            if(date[1] != day){
+            if(date[1] != day){ //if today is new day, refresh the log to see new inputs
                 refresh_iframe();
+                day = date[1];
             }
 //            console.log("UTC TIME: ", datetime);
-            delete data.date;
+            delete data.date; //remove first point
 //            console.log(data);
-            for(var x in data){
+            for(var x in data){ //make an array of points the number of Sensor data in the json object
                 points[c] = [datetime, data[x]];
                 c++;
 //                console.log(points[c]);
             } //c = NUM_SENSORS NOW
             for(var i = 0; i < c; i++){
-                if (chart.series.length == i){
+                if (chart.series.length == i){ //If there is not a currently existing series for that series, create a new one
                     chart.addSeries({
                         name: ('Sensor ' + i),
                         data: [],
@@ -46,24 +58,25 @@ function update_chart_table() {
                             radius:1}
                     });
                 }
-                chart.series[i].addPoint(points[i], true, false);
-                if (chart.series[i].data.length > 3360){
+                chart.series[i].addPoint(points[i], true, false); //Add the data point to the corresponding series
+                if (chart.series[i].data.length > (days_to_keep_live_data*3600*24*1000/chart_refresh_time)){ //If past days to keep live data, remove first point
                     chart.series[i].removePoint(0, true);
                 }
             }
-            update_table();
+            update_table(); //refesh table
 
         },
         cache: false
     })
 };
 
-function refresh_iframe(){
+function refresh_iframe(){ //resets the frames source to refresh the iframe
     var ifr=document.getElementsByName('arduino_history')[0];
     ifr.src=ifr.src;
 }
 
-
+//Sends request to arduino to update live data chart
+var live_table
 function update_table() {
     $.ajax({
         url: "http://192.168.100.100/json",
@@ -72,11 +85,13 @@ function update_table() {
         timeout: 3000,
         success: function(data){
             delete data.date;
-            var live_table = '<table><tr>';
+            live_table = '<table><tr>';
+            //set the headers
             for(var i in data){
                 live_table += ('<th>Sensor ' + i[1] + '</th>');
             }
             live_table += '</tr><tr>';
+            //check if data is within the thresholds and color them accordingly
             for(var i in data){
                 if(data[i] > upper_thresh || data[i] < lower_thresh){
                     live_table += '<td class=red>' + data[i] + '&deg;C</td>';
@@ -86,7 +101,7 @@ function update_table() {
                 }
             }
             live_table += '</tr></table>';
-            document.getElementById('live_temp_table').innerHTML = live_table;
+            document.getElementById('live_temp_table').innerHTML = live_table; //Create new table with current data
             
             
             
@@ -103,7 +118,7 @@ function update_table() {
 };
 
 var all_ips = [], bad_ips = [];
-
+//from local directory, read the text files and split them by line
 function get_ip_list(){
     $.get('iplist.txt', function(data){
         all_ips = [];
@@ -115,57 +130,46 @@ function get_ip_list(){
     });
     display_iplist();
 }
-            
+
+
+
+var iptable = '';
+var isbad = false;
 function display_iplist(){
     //BAD IP TXT FILE NEEDS NEW LINE AT THE END OR LAST ONE WILL NOT WORK CORRECTLY
-    //console.log(all_ips, bad_ips);
-//    console.log(all_ips)
-    var iptable = "<table><tr><th colspan=\"12\">IP Addresses</th></tr><tr>";
-    var isbad = false;
+    iptable = "<table><tr><th colspan=\"" + num_ip_colums + "\">IP Addresses</th></tr><tr>";
+    isbad = false;
     
-    for (var i = 0; i < all_ips.length - 1; i++){
-        if ( i % 12 == 0 && i != 0){
+    for (var i = 0; i < all_ips.length - 1; i++){ //table formatting
+        if ( i % num_ip_colums == 0 && i != 0){
             iptable += "</tr><tr>";
         }
-        for (var j = 0; j < bad_ips.length; j++){
+        for (var j = 0; j < bad_ips.length; j++){ //iterate through both lists to compare and check for matches
 //            console.log(all_ips[i], "compared to: ", bad_ips[j]);
             if (all_ips[i].localeCompare(bad_ips[j]) == 0){
                 isbad = true;
                 break;
             }
         }
-        
+        //Color each ip accordingly, if the ip doesnt have a lenght, just add blank column
         if (all_ips[i].length == 0){
             iptable += "<td>" + all_ips[i]+ "</td>";
         }
         else if(isbad){
             iptable += "<td class=\"red\">" + all_ips[i]+ "</td>";
         } 
-//        else if (all_ips[i].length == 0){
-//            iptable += "<td>" + all_ips[i]+ "</td>";
-//        }
         else{
             iptable += "<td class=\"green\">" + all_ips[i]+ "</td>";
         }
         isbad = false;
     }
-    
-    
-//    var c = all_ips.length;
-//    console.log(c%4);
-//    if(c%4 > 0){
-//        while((c % 4) != 0){
-//            iptable+= "<td></td>";
-//            c++;
-//        }
-//    }
+
     iptable += "</tr></table>"
     //console.log(iptable);
-    document.get
-    document.getElementById('iptable').innerHTML = iptable;
+    document.getElementById('iptable').innerHTML = iptable; //set the table
 }
             
-            
+//Set the intervales for updating different elements and button coloring/funciton
 function start_running(){
     document.getElementById("s_button").innerHTML = "Stop Updating";
     document.getElementById("s_button").style.backgroundColor = "#FF4136";
@@ -183,6 +187,8 @@ window.onload = function (){
     get_ip_list();
 }
 
+
+//stops elements from updating
 function stop_running(){
     clearInterval(chart_interval);
     clearInterval(table_interval);
@@ -192,7 +198,7 @@ function stop_running(){
     document.getElementById("s_button").setAttribute('onClick', 'start_running();')
     
 }
-
+//when ready, create the highcharts options
 $(document).ready(function() {
     // define the options
     var options = {
